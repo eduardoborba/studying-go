ccbd3ac8b8cf39810ca5fa290fdf75abf4fce3f5
Eduardo Borba <eduardo@jobscore.com>
2021-01-23 21:14:31 -0300

    Fix identation

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"encoding/json"
-	"net/http"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
@@ -14,56 +14,56 @@ import (
 
 type Article struct {
 	gorm.Model
-    Title string
-    Desc string
-    Content string
+	Title   string
+	Desc    string
+	Content string
 }
 
 var db *gorm.DB
 var err error
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func getArticles(w http.ResponseWriter, r *http.Request){
+func getArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: getArticles")
 	var articles []Article
-	
+
 	db.Find(&articles)
-    
+
 	json.NewEncoder(w).Encode(&articles)
 }
 
-func getArticle(w http.ResponseWriter, r *http.Request){
-    params := mux.Vars(r)
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 	var article Article
 
 	db.First(&article, params["id"])
 
-    json.NewEncoder(w).Encode(&article)
+	json.NewEncoder(w).Encode(&article)
 }
 
 func createArticle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: createArticle")
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var article Article 
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
 	json.Unmarshal(reqBody, &article)
-	
+
 	db.Create(&article)
 
-    json.NewEncoder(w).Encode(article)
+	json.NewEncoder(w).Encode(article)
 }
 
 func deleteArticle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: deleteArticle")
-    params := mux.Vars(r)
+	params := mux.Vars(r)
 	var article Article
-	
+
 	db.First(&article, params["id"])
 	db.Delete(&article)
-	  
+
 	json.NewEncoder(w).Encode(&article)
 }
 
@@ -71,7 +71,7 @@ func updateArticle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: updateArticle")
 	params := mux.Vars(r)
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var updatedArticle Article 
+	var updatedArticle Article
 	var article Article
 	json.Unmarshal(reqBody, &updatedArticle)
 
@@ -89,7 +89,7 @@ func handleRequests() {
 	router.HandleFunc("/articles/{id}", updateArticle).Methods("PUT")
 	router.HandleFunc("/articles/{id}", getArticle).Methods("GET")
 	router.HandleFunc("/articles/{id}", deleteArticle).Methods("DELETE")
-	
+
 	log.Fatal(http.ListenAndServe(":10000", router))
 }
 
@@ -104,6 +104,6 @@ func main() {
 	if err != nil {
 		fmt.Println("Error HandleMigrate:" + err.Error())
 	}
-	
-	handleRequests()	
+
+	handleRequests()
 }
